3be0708e34a1a5e7e4beb87e2d849bf47aa17e1c
Toolback <93286025+Toolback@users.noreply.github.com>
2022-12-01 21:57:04 +0100

    Initial commit

---
+++ Template_AuthAPI_Express-Mongoose-main/Modals/usermodal.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    postingKey: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+UserSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        //do not reveal passwordHash
+        delete returnedObject.password
+        delete returnedObject.postingKey
+
+    }
+})
+
+const User =  mongoose.model("user", UserSchema);
+
+module.exports = User;
